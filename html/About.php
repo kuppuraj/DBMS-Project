d28a2cc8846c2a1f8b819599eb443cfc7d30f838
prashanth07 <prashanth07@gmail.com>
2012-03-27 11:10:56 -0400

    Added About page

---
+++ html/About.php
@@ -0,0 +1,26 @@
+#!/usr/local/bin/php
+<?php 
+  require_once 'template.php'; 
+  echo head('About'); 
+?>
+<h1>eMart</h1>
+<h4>This is a final course project of <a href="http://www.cise.ufl.edu/~mschneid/Teaching/COP5725_Spring2012/COP5725_Spring2012.html">COP5725 - Database Management System - Spring 2012</a></h4>
+
+<p><b>Instructor:</b>
+<li><a href="http://www.cise.ufl.edu/~mschneid/">Dr. Markus Schneider</a></li>
+</p>
+
+<p><b>Teaching Assistant: </b>
+<li><a href="http://www.cise.ufl.edu/~nanguyen/">Nam Nguyen</a></li>
+</p>
+
+<p>
+<b>Team Members:  </b>
+<li><a href="http://www.hcs.ufl.edu/~prakash/">Prashanth Prakash</a></li>
+<li>Bharath C</li>
+<li>Kuppuraj Karthik</li>
+<li>Sandilya S</li>
+</p>
+<?php 
+  echo foot(); 
+?>
